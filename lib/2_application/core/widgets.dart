97f6ed62674ad27b850854b7e09df0d04a81bfb5
PandaHunterX <147257813+PandaHunterX@users.noreply.github.com>
2024-09-08 23:58:31 +0800

    Added new Widgets

--- lib/2_application/core/widgets.dart
+++ lib/2_application/core/widgets.dart
@@ -1,5 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:reflectica/2_application/pages/book_page/books.dart';
 import 'package:reflectica/styles.dart';
+
+class Main extends StatelessWidget {
+  const Main({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: secondaryGrey,
+          title: Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                    width: 99,
+                    height: 101,
+                    child: Image.asset(
+                      appIcon,
+                      fit: BoxFit.cover,
+                    )),
+                const Text(
+                  'Reflectica',
+                  style: textHeader,
+                )
+              ],
+            ),
+          ),
+          toolbarHeight: 127,
+        ),
+        backgroundColor: appColor,
+        body: const Column(
+          children: [
+            NavBar(),
+            BookPage()
+          ],
+        )
+      ),
+    );
+  }
+}
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
@@ -16,8 +62,14 @@ class NavBar extends StatelessWidget {
       child: const Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Navbar_Tab(icon: Icons.backpack, text: 'Books',),
-          Navbar_Tab(icon: Icons.stars_sharp, text: 'Fallacies',)
+          Navbar_Tab(
+            icon: Icons.backpack,
+            text: 'Books',
+          ),
+          Navbar_Tab(
+            icon: Icons.stars_sharp,
+            text: 'Fallacies',
+          )
         ],
       ),
     );
@@ -25,7 +77,8 @@ class NavBar extends StatelessWidget {
 }
 
 class Navbar_Tab extends StatelessWidget {
-  const Navbar_Tab({ super.key, required this.icon, required this.text});
+  const Navbar_Tab({super.key, required this.icon, required this.text});
+
   final IconData icon;
   final String text;
 
@@ -38,9 +91,17 @@ class Navbar_Tab extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Icon(icon, size: 44,),
-          Text(text, style: const TextStyle(fontFamily: 'Montserrat',
-            fontSize: 24,),),
+          Icon(
+            icon,
+            size: 44,
+          ),
+          Text(
+            text,
+            style: const TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 24,
+            ),
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'styles.dart';
 import '2_application/core/widgets.dart';
 void main() {
   runApp(const HomePage());
@@ -10,34 +9,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: secondaryGrey,
-          title: Padding(
-            padding: const EdgeInsets.only(right: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
-                    width: 99,
-                    height: 101,
-                    child: Image.asset(
-                      appIcon,
-                      fit: BoxFit.cover,
-                    )),
-                const Text(
-                  'Reflectica',
-                  style: textHeader,
-                )
-              ],
-            ),
-          ),
-          toolbarHeight: 127,
-        ),
-        body: const NavBar(),
-      ),
-    );
+    return const Main();
   }
 }
+
